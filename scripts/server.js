9c7f4edd2452e870bb802556c290a98781a041f1
Nick Masciandaro <nmash1937@icloud.com>
2019-11-20 15:08:37 -0500

    Update server.js
    Now the server can make multiple queries

--- scripts/server.js
+++ scripts/server.js
@@ -1,53 +1,30 @@
 var express = require('express');
 var app = express();
 const fs = require('fs')
+var sql = require("mssql");
+var sql2 = require("mssql");
 
+var config = {
+    user: 'sa',
+    password: 'galexy2019!',
+    server: 'localhost',
+};
 
 // config for your database
 
 
 app.get('/', function (req, res) {
-    var sql = require("mssql");
 
-    var config = {
-        user: 'sa',
-        password: 'galexy2019!',
-        server: 'localhost',
-    };
 
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // connect to your database
-    /*
-    async () => {
-
-            // make sure that any items are correctly URL encoded in the connection string
-            await sql.connect(config)
-            const result = await sql.query `sselect * from q_table.dbo.[Move] where id = 0`
-            console.dir(result)
-
-    }*/
-
-
-     console.log("0")
-    sql.connect(config, function (err) {
-        console.log("1")
-        if (err) console.log(err);
-        console.log("2")
-        // create Request object
-        var request = new sql.Request();
-    console.log("3")
-        // query to the database and get the records
-        request.query('select * from q_table.dbo.[Move]', function (err, recordset) {
-             console.log("4")
-            if (err) console.log(err)
-            // send records as a response
-             console.log("5")
-            res.send(recordset);
-             console.log("6")
-            console.log('worked')
-        });
-    });
+    sql2.connect(config).then(pool => {
+        return pool.request().query('select * from q_table.dbo.[Move]')
+    }).then(result =>{
+        res.send(result);
+    }).catch(err => {
+        console.log("There was an error")
+     })
 
 });
 
